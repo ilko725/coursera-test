3a3594196b7e0d1a1bb9438671781ea89f86f7b4
ilko725 <ilko725@gmail.com>
2017-01-24 00:57:58 +0200

    start working

---
+++ assignment1/JS/app.js
@@ -0,0 +1,42 @@
+(function () {
+'use strict';
+
+  angular.module('courseraAssignment1', [])
+
+  .controller('assignment1', assignment1);
+
+  assignment1.$inject = ['$scope', '$filter'];
+  function assignment1($scope, $filter) {
+    $scope.name = "";
+    $scope.check = function checkIfTooMuch() {
+
+    };
+    $scope.upper = function () {
+      var upCase = $filter('uppercase');
+      $scope.name = upCase($scope.name);
+    };
+  };
+
+  /*.controller('MyFirstController', function ($scope) {
+    $scope.name = "";
+    $scope.totalValue = 0;
+    $scope.sayHello = function () {
+      return "Hello Coursera";
+    }
+    $scope.displayNumeric = function () {
+      var totalNameValue = calculateNumericForString($scope.name); //get the total value
+      $scope.totalValue = totalNameValue;
+    };
+
+
+    function calculateNumericForString(string) {
+      var totalStringValue = 0;
+      for (var i = 0; i < string.length; i++) {
+        totalStringValue += string.charCodeAt(i);
+      }
+
+      return totalStringValue;
+    };
+  });*/
+
+})();
